be4103debf904dac4d639ef7019854e0b1ee343a
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2017-07-17 01:23:11 -0700

    MVP-Added Alert System & Tweaked The Way The API Response Is Handled
    Also updated gitignore

--- js/script.js
+++ js/script.js
@@ -1,47 +1,75 @@
-function displayFroYo(){
+function displayFroYo(langs, repoName, repoOwner){
   var canvasDiv = document.getElementById("canvasDiv");
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
+
+  if(canvasDiv.style.display == "none"){
+    canvasDiv.style.display = "inline";
+    loadCanvas(canvas, context, langs, repoName, repoOwner);
+  }else{
+    clearCanvas(context);
+    loadCanvas(canvas, context, langs, repoName, repoOwner);
+  }
+}
+
+function froYoMyRepo(){
+  var input = processInput();
+  var langs;
+  var alert = document.getElementById("alert");
+  if(input.length != 2){
+    //Invalid input
+  }else{
+    console.log("input = " +input);
+    getLangsFromGitHub(input[0],input[1], function(langArray){
+      langs = langArray;
+      console.log("langs = " +langs);
+      if(langs.length > 0){
+          console.log("Displaying FroYo......");
+          //displayFroYo(langs, input[1], input[0]);
+      }else{
+        alert.style.display = "inline";
+      }
+    });
+  }
+}
+
+function processInput(){
   var inputMethod1RadioButton = document.getElementById("inputMethod1");
-  var inputMethod2RadioButton = document.getElementById("inputMethod2");
-  var urlInput = document.getElementById("repoUrl");
+  //var inputMethod2RadioButton = document.getElementById("inputMethod2");
+  //var urlInput = document.getElementById("repoUrl");
   var repoOwnerInput = document.getElementById("repoOwner");
   var repoNameInput = document.getElementById("repoName");
-  var repoName = "";
-  var repoOwner = "";
-  var langArray = [];
+  var input = [];
 
   if(inputMethod1RadioButton.checked){
   }else{
     if(repoOwnerInput.value != "" && repoNameInput.value != ""){
-      getLangsFromGitHub(repoOwnerInput.value, repoNameInput.value);
+      input.push(repoOwnerInput.value.trim());
+      input.push(repoNameInput.value.trim());
     }
   }
-
-  if(canvasDiv.style.display == "none"){
-    canvasDiv.style.display = "inline";
-    loadCanvas(canvas, context);
-  }else{
-    clearCanvas(context);
-    loadCanvas(canvas, context);
-  }
+  return input;
 }
 
-function getLangsFromGitHub(ownerName, repo){
+function getLangsFromGitHub(ownerName, repo, callback){
   var gitHubApiEndpointUrl = "https://api.github.com";
-
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       console.log(xhttp.responseText);
-    }else{
-      //Something went wrong.
-    }
-  };
-  https://api.github.com/repos/ZacharyDavidSaunders/bubo/languages
+  var langs = [];
+  // https://api.github.com/repos/ZacharyDavidSaunders/bubo/languages
   xhttp.open("GET", (gitHubApiEndpointUrl+"/repos/"+ownerName+"/"+repo+"/languages"), true);
   console.log(gitHubApiEndpointUrl+"/repos/"+ownerName+"/"+repo+"/languages");
   xhttp.send();
+
+  xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            langs = Object.keys(response);
+            console.log("langs: "+langs);
+        }else {
+            //Something went wrong.
+        }
+      callback(langs);
+  };
 }
 
 function useInputMethod2(){
@@ -70,7 +98,7 @@ function useInputMethod1(){
   }
 }
 
-function loadCanvas(canvas, context){
+function loadCanvas(canvas, context, langs,  repoName, repoOwner){
   var middleWidth = canvas.width / 2;
   var cone = new Image();
   cone.src = "../imgs/froYoImages/cone.png";
