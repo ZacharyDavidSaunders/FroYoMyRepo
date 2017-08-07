45f9c467e6076316c616cde4a7255b30352f5dac
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2017-08-06 21:27:53 -0700

    MVP-Removed Alert System and Completed MVP

--- js/script.js
+++ js/script.js
@@ -2,34 +2,23 @@ function displayFroYo(langs, repoName, repoOwner){
   var canvasDiv = document.getElementById("canvasDiv");
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
-
-  if(canvasDiv.style.display == "none"){
-    canvasDiv.style.display = "inline";
-    loadCanvas(canvas, context, langs, repoName, repoOwner);
-  }else{
-    clearCanvas(context);
-    loadCanvas(canvas, context, langs, repoName, repoOwner);
-  }
+  canvasDiv.style.display = "inline";
+  loadCanvas(canvas, context, langs, repoName);
 }
 
 function froYoMyRepo(){
   var input = processInput();
-  var langs;
-  var alert = document.getElementById("alert");
-  if(input.length != 2){
-    //Invalid input
+
+  if(input){
+      console.log("input = " +input);
+      getLangsFromGitHub(input[0],input[1], function(langArray){
+          if(typeof (langArray) ==="object") {
+              var langs = langArray;
+              console.log("langs = " + langs);
+              displayFroYo(langs, input[1], input[0]);
+          }
+      });
   }else{
-    console.log("input = " +input);
-    getLangsFromGitHub(input[0],input[1], function(langArray){
-      langs = langArray;
-      console.log("langs = " +langs);
-      if(langs.length > 0){
-          console.log("Displaying FroYo......");
-          //displayFroYo(langs, input[1], input[0]);
-      }else{
-        alert.style.display = "inline";
-      }
-    });
   }
 }
 
@@ -39,13 +28,15 @@ function processInput(){
   //var urlInput = document.getElementById("repoUrl");
   var repoOwnerInput = document.getElementById("repoOwner");
   var repoNameInput = document.getElementById("repoName");
-  var input = [];
 
   if(inputMethod1RadioButton.checked){
+    //Parse URL and locate owner and repo name
   }else{
-    if(repoOwnerInput.value != "" && repoNameInput.value != ""){
+    if(repoOwnerInput.value && repoNameInput.value){
+      var input = [];
       input.push(repoOwnerInput.value.trim());
       input.push(repoNameInput.value.trim());
+    }else{
     }
   }
   return input;
@@ -54,21 +45,20 @@ function processInput(){
 function getLangsFromGitHub(ownerName, repo, callback){
   var gitHubApiEndpointUrl = "https://api.github.com";
   var xhttp = new XMLHttpRequest();
-  var langs = [];
-  // https://api.github.com/repos/ZacharyDavidSaunders/bubo/languages
-  xhttp.open("GET", (gitHubApiEndpointUrl+"/repos/"+ownerName+"/"+repo+"/languages"), true);
-  console.log(gitHubApiEndpointUrl+"/repos/"+ownerName+"/"+repo+"/languages");
+
+  xhttp.open("GET", (gitHubApiEndpointUrl+"/repos/"+ownerName+"/"+repo+"/languages"), true); // https://api.github.com/repos/ZacharyDavidSaunders/bubo/languages
+  console.log("API request = " + gitHubApiEndpointUrl+"/repos/"+ownerName+"/"+repo+"/languages");
   xhttp.send();
 
   xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(xhttp.responseText);
-            langs = Object.keys(response);
-            console.log("langs: "+langs);
-        }else {
-            //Something went wrong.
+            var langs = Object.keys(response);
+            callback(langs);
+        }else if(this.readyState == 4){
+            console.log("readyState: " +this.readyState);
+            console.log("status: " +this.status);
         }
-      callback(langs);
   };
 }
 
@@ -98,14 +88,41 @@ function useInputMethod1(){
   }
 }
 
-function loadCanvas(canvas, context, langs,  repoName, repoOwner){
+function loadCanvas(canvas, context, langs, repoName){
   var middleWidth = canvas.width / 2;
   var cone = new Image();
+  var randomNumber;
+  var scoops = [];
+  var possibleScoopSrcs = ["../imgs/froYoImages/blueScoop.png","../imgs/froYoImages/greenScoop.png","../imgs/froYoImages/brownScoop.png","../imgs/froYoImages/purpleScoop.png","../imgs/froYoImages/yellowScoop.png","../imgs/froYoImages/redScoop.png"];
+  context.font = "bold 30pt Verdana";
+  context.textAlign= "center";
+  context.lineWidth = 1.8;
+  context.strokeStyle = 'white';
+  context.fillStyle = "black";
   cone.src = "../imgs/froYoImages/cone.png";
-
-  cone.onload = function () {
-    context.drawImage(cone, middleWidth-60, 350, cone.width/8, cone.height/8);
+  for(var i = 0; i < langs.length; i++){
+      randomNumber = Math.floor(Math.random() * possibleScoopSrcs.length);
+      scoops.push(new Image());
+      scoops[i].src = possibleScoopSrcs[randomNumber];
   }
+  console.log("Scoops length = "+scoops.length);
+  displayCanvas(cone, scoops, context, middleWidth, repoName, langs);
+}
+
+function displayCanvas(cone, scoops, context, middleWidth, repoName, langs){
+        var scoopOffset = cone.height/8;
+        var langLabelOffset = scoopOffset*1.5;
+        context.drawImage(cone, middleWidth-60, 350, cone.width/8, cone.height/8);
+        context.fillText(repoName, cone.width/6 + 5, cone.height/4);
+        context.strokeText(repoName, cone.width/6 + 5, cone.height/4);
+        context.font = "bold 20pt Verdana";
+        for(var i =0; i < scoops.length; i++) {
+            context.drawImage(scoops[i], 100, scoopOffset, scoops[i].width/8, scoops[i].height/8);
+            context.fillText(langs[i], 180, langLabelOffset);
+            context.strokeText(langs[i], 180, langLabelOffset);
+            scoopOffset-= 80;
+            langLabelOffset-=80;
+        }
 }
 
 function clearCanvas(context){
